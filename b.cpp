0295d1b58bcc358b2f78a6265a52a9c48897abe0
unknown <daisicheng@cn.net.ntes>
2021-08-05 14:12:20 +0800

    update

--- b.cpp
+++ b.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main()
 {
     cout << 'haofdk' << endl;
+    cout << 'fhdkd' << endl;
     return 0;
 }
--- c.cpp
+++ c.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    cout << 'fdksk' << endl;
     return 0;
 }

f18df5253a611f3e015cd8703d048621783d1e02
unknown <daisicheng@cn.net.ntes>
2021-08-05 11:08:32 +0800

    git status

--- learn.cpp
+++ learn.cpp
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<iostream>
+#include<vector>
+using namespace std;
 int main(){
+    vector<int>res(5);
+    res[0] = 1;
     std::cout << "Sicheng loves Dengbin" << std::endl;
     return 0;
 }
